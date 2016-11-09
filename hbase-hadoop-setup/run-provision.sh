fdcdcaae8d831687eaa8378cb1dd8158b33be7ec
Eadred <eadred.birchenough@zuhlke.com>
2016-11-09 11:15:37 +0000

    Provisioning logging now captures stderr (in a different file)

--- hbase-hadoop-setup/run-provision.sh
+++ hbase-hadoop-setup/run-provision.sh
@@ -1,6 +1,11 @@
 SCRIPTDIR=$(dirname $0)
-if [ -f $SCRIPTDIR/provision.log ]
+if [ -f $SCRIPTDIR/provision.out ]
 then
-  rm $SCRIPTDIR/provision.log
+  rm $SCRIPTDIR/provision.out
 fi
-$SCRIPTDIR/provision.sh | tee $SCRIPTDIR/provision.log
+if [ -f $SCRIPTDIR/provision.err ]
+then
+  rm $SCRIPTDIR/provision.err
+fi
+
+$SCRIPTDIR/provision.sh > >(tee $SCRIPTDIR/provision.out) 2> >(tee $SCRIPTDIR/provision.err >&2)
