a49bb78acd805656b655bfafe6093a65c773fd3b
Eadred <eadred.birchenough@zuhlke.com>
2016-11-09 12:14:09 +0000

    Getting java to install silently

--- hbase-hadoop-setup/provision.sh
+++ hbase-hadoop-setup/provision.sh
@@ -23,8 +23,11 @@ fi
 echo "Checkpoint: Provisioning..."
 
 echo "Checkpoint: Installing base packages"
-sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y ssh
 sudo apt-get install -y rsync
