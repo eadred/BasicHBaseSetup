0acb67bfcd5918d5946f4e35f27f650ebd024e01
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-11-15 12:57:05 +0000

    Added logging for mysql install script

--- oozie-setup/mysql-install.sh
+++ oozie-setup/mysql-install.sh
@@ -1,8 +1,20 @@
-echo "Install and set mysql server ......................................... "
+
+LOG_DIRECTORY=/home/oozie/logs
+LOG_FILE=$LOG_DIRECTORY/mysql-install.log
+if [ ! -d "$LOG_DIRECTORY" ]; then
+	sudo mkdir -p $LOG_DIRECTORY	
+fi
+
+if [ -e "$LOG_FILE" ]; then
+	sudo rm $LOG_FILE
+fi
+
+echo "Install and set mysql server ......................................... " > $LOG_FILE
 # Download and Install the Latest Updates for the OS
+
 sudo apt-get update  -y
 # Install MySQL Server in a Non-Interactive mode. No password for root
 export DEBIAN_FRONTEND="noninteractive"
 echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
-apt-get -y install mysql-server
+apt-get -y install mysql-server > $LOG_FILE
